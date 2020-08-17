df098ccaa846b40231ac987b33738d775cf3c192
Promise Izuagbala <izuagbalapromise@gmail.com>
2020-08-17 14:00:34 +0100

    aDDED Port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { app } from './app';
 
 const start = async () => {
-  app.listen(3000, () => {
+  app.listen(process.env.PORT, () => {
     console.log('Listening on port 3000!');
   });
 };
